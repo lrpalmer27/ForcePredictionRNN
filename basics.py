2280b384db5ab8656984a1bd19d4b6114e8f0068
lrpalmer27 <lrpalmer@mun.ca>
2023-12-01 16:08:26 -0330

    initial

---
+++ basics.py
@@ -0,0 +1,18 @@
+### This file just has some basics that might be useful later during devel. 
+
+Filename="12p5m_9ths_0p5kts_0p6m_0deg_001.csv"
+data_list = [
+    'FrameN', 'Carriage_Speed_DP', 'Global_FX', 'Global_FY', 'Global_FT', 'Global_MZ',
+    'Floe_Size', 'Drift_Speed', 'Ice_Thick', 'Drift_Angle',
+    'Region_0:228.0_0:20 - Ice Conc', 'Region_0:228.0_0:20 - Mean', 'Region_0:228.0_0:20 - StdDev',
+    'Region_0:228.0_20:60 - Ice Conc', 'Region_0:228.0_20:60 - Mean', 'Region_0:228.0_20:60 - StdDev',
+    'Region_0:228.0_60:135 - Ice Conc', 'Region_0:228.0_60:135 - Mean', 'Region_0:228.0_60:135 - StdDev',
+    'Region_0:228.0_135:180 - Ice Conc', 'Region_0:228.0_135:180 - Mean', 'Region_0:228.0_135:180 - StdDev',
+    'Region_228.0:456_0:20 - Ice Conc', 'Region_228.0:456_0:20 - Mean', 'Region_228.0:456_0:20 - StdDev',
+    'Region_228.0:456_20:60 - Ice Conc', 'Region_228.0:456_20:60 - Mean', 'Region_228.0:456_20:60 - StdDev',
+    'Region_228.0:456_60:135 - Ice Conc', 'Region_228.0:456_60:135 - Mean', 'Region_228.0:456_60:135 - StdDev',
+    'Region_228.0:456_135:180 - Ice Conc', 'Region_228.0:456_135:180 - Mean', 'Region_228.0:456_135:180 - StdDev',
+    'Region_456:1140.0_0:20 - Ice Conc', 'Region_456:1140.0_0:20 - Mean', 'Region_456:1140.0_0:20 - StdDev',
+    'Region_456:1140.0_20:60 - Ice Conc', 'Region_456:1140.0_20:60 - Mean', 'Region_456:1140.0_20:60 - StdDev',
+    'Region_456:1140.0_60:135 - Ice Conc', 'Region_456:1140.0_60:135 - Mean', 'Region_456:1140.0_60:135 - StdDev',
+    'Region_456:1140.0_135:180 - Ice Conc', 'Region_456:1140.0_135:180 - Mean', 'Region_456:1140.0_135:180 - StdDev']
